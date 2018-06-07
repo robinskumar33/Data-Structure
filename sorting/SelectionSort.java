711174d548c51ecb635dda743ceff7e4bd494f02
Robins Kumar <robins.kumar@hashworks.co>
2018-06-07 18:45:48 +0530

    Selection sort added

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+
+public class SelectionSort {
+
+	public static void main(String[] args)
+	{
+		int arr[]={56,5,33,24};
+		for (int i = 0; i < arr.length; i++) 
+		{
+			//int least=arr[i];
+			for (int j =i+1; j < arr.length; j++) 
+			{
+				if(arr[j]<arr[i])
+				{
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+					
+			}
+		}
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+}
